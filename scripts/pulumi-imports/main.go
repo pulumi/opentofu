5c3cd62e8b734266772137a0255725d910e393ff
Anton Tayanovskyy <anton@pulumi.com>
2025-02-26 19:05:33 -0500

    Add a tool to rewrite imports

---
+++ scripts/pulumi-imports/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bytes"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func main() {
+	var buf bytes.Buffer
+	cmd := exec.Command("git", "ls-files", "**/*.go")
+	cmd.Stdout = &buf
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, f := range strings.Split(buf.String(), "\n") {
+		f = strings.TrimSpace(f)
+		if f == "" {
+			continue
+		}
+
+		origBytes, err := os.ReadFile(f)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		search := []byte("github.com/opentofu/opentofu/internal")
+		repl := []byte("github.com/pulumi/opentofu/internal")
+
+		newBytes := bytes.ReplaceAll(origBytes, search, repl)
+
+		if err := os.WriteFile(f, newBytes, 0700); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+}
